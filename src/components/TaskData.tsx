b1666a5f4da477afbdcb05dafccc5020bf5e6971
ItsikEdri <itsik.edri@metropolinet.co.il>
2021-07-29 18:18:06 +0300

    changes

--- src/components/TaskData.tsx
+++ src/components/TaskData.tsx
@@ -27,42 +27,7 @@ import FilterListIcon from "@material-ui/icons/FilterList";
 import axios from "axios";
 import { Button, ListItemIcon } from "@material-ui/core";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
-
 import {Data} from '../interfaces/tasks-data.interface';
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
 
 interface HeadCell {
   disablePadding: boolean;
@@ -70,6 +35,25 @@ interface HeadCell {
   label: string;
   numeric: boolean;
 }
+
+interface EnhancedTableProps {
+  classes: ReturnType<typeof useStyles>;
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+
+type Order = "asc" | "desc";
 
 const headCells: HeadCell[] = [
   { id: "name", numeric: false, disablePadding: true, label: "Name" },
@@ -108,17 +92,134 @@ const headCells: HeadCell[] = [
   },
 ];
 
-interface EnhancedTableProps {
-  classes: ReturnType<typeof useStyles>;
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
+const useToolbarStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+    },
+    highlight:
+      theme.palette.type === "light"
+        ? {
+            color: theme.palette.secondary.main,
+            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+          }
+        : {
+            color: theme.palette.text.primary,
+            backgroundColor: theme.palette.secondary.dark,
+          },
+    title: {
+      flex: "1 1 100%",
+    },
+  })
+);
+const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          className={classes.title}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          className={classes.title}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton aria-label="filter list">
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      minWidth: '70vw'
+    },
+    paper: {
+      width: "100%",
+      marginBottom: theme.spacing(2),
+    },
+    table: {
+      minWidth: 750,
+    },
+    assignDiv: {
+      display: "flex",
+      justifyContent: "flex-start",
+      padding: "0.5rem",
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: "rect(0 0 0 0)",
+      height: 1,
+      margin: -1,
+      overflow: "hidden",
+      padding: 0,
+      position: "absolute",
+      top: 20,
+      width: 1,
+    },
+  })
+);
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
 }
 
 function EnhancedTableHead(props: EnhancedTableProps) {
@@ -173,115 +274,12 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   );
 }
 
-const useToolbarStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(1),
-    },
-    highlight:
-      theme.palette.type === "light"
-        ? {
-            color: theme.palette.secondary.main,
-            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-          }
-        : {
-            color: theme.palette.text.primary,
-            backgroundColor: theme.palette.secondary.dark,
-          },
-    title: {
-      flex: "1 1 100%",
-    },
-  })
-);
-
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          className={classes.title}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          className={classes.title}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Tasks
-        </Typography>
-      )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton aria-label="filter list">
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: "100%",
-      minWidth: '70vw'
-    },
-    paper: {
-      width: "100%",
-      marginBottom: theme.spacing(2),
-    },
-    table: {
-      minWidth: 750,
-    },
-    assignDiv: {
-      display: "flex",
-      justifyContent: "flex-start",
-      padding: "0.5rem",
-    },
-    visuallyHidden: {
-      border: 0,
-      clip: "rect(0 0 0 0)",
-      height: 1,
-      margin: -1,
-      overflow: "hidden",
-      padding: 0,
-      position: "absolute",
-      top: 20,
-      width: 1,
-    },
-  })
-);
-
 interface TableProps {
   dataRows: Data[];
+  setButtons: () => void;
 }
 
-const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
+const EnhancedTable: React.FC<TableProps> = ({dataRows,setButtons}) => {
   const classes = useStyles();
   const [order, setOrder] = React.useState<Order>("asc");
   const [orderBy, setOrderBy] = React.useState<keyof Data>("type");
@@ -304,7 +302,6 @@ const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
       const newSelected = dataRows.map((n) => n.id);
-      debugger;
       setSelected(newSelected);
       return;
     }
@@ -312,9 +309,9 @@ const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
   };
 
   const handleClick = (event: React.MouseEvent<unknown>, id: string) => {
+    debugger
     const selectedIndex = selected.indexOf(id);
     let newSelected: string[] = [];
-
     if (selectedIndex === -1) {
       newSelected = newSelected.concat(selected, id);
     } else if (selectedIndex === 0) {
@@ -327,8 +324,8 @@ const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
         selected.slice(selectedIndex + 1)
       );
     }
-
     setSelected(newSelected);
+    setButtons();
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -348,72 +345,33 @@ const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
 
   const isSelected = (id: string) => selected.indexOf(id) !== -1;
 
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, dataRows.length - page * rowsPerPage);
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, dataRows.length - page * rowsPerPage);
 
-  // Manipulate Data
-  // Set variables
-  // const baseUrl = "http://localhost:8000/gateway/v1/";
-
-  // const [agent, setAgent] = React.useState<{
-  //   agent_name: string;
-  //   agent_id: string;
-  // }>({
-  //   agent_name: "",
-  //   agent_id: "",
-  // });
-
-  // const fetchTasksData = async () => {
-  //   const response = await axios.get(`${baseUrl}tasks/systems/3`, {
-  //     headers: {
-  //       Authorization:
-  //         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MGU1YjI4NTQ3YTBmMDAwMmZiNWUzYzkiLCJpYXQiOjE2MjY2MDk0ODIsImV4cCI6MTYyOTIwMTQ4Mn0.gLHb_V-9eTBlhoMtb1OD6GuHv4oTcbLatufJ5WXMnU8",
-  //     },
-  //   });
-
-  //   const tasks: Task[] = response.data.data.tasks || [];
-  //   const modifiedTasks = tasks.map((task: Task) => {
-  //     return createData(
-  //       task.id,
-  //       task.name,
-  //       task.type,
-  //       task.state.stateHistory[0].currentState,
-  //       task.description,
-  //       task.start_time,
-  //       task.duration,
-  //       task.assignment_info?.agent_name || "task is not assigned",
-  //       task.created_at,
-  //       task.updated_at
-  //     );
-  //   });
-  //   setDataRows(modifiedTasks);
-  // };
 
   const assignTask = async () => {
+    //Open full modal screen
     const agentName = window.prompt("Enter agent name <dev>:");
     const agentId = window.prompt("Enter agent id <dev>:");
 
     const config = {
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MGU1YjI4NTQ3YTBmMDAwMmZiNWUzYzkiLCJpYXQiOjE2MjY2MDk0ODIsImV4cCI6MTYyOTIwMTQ4Mn0.gLHb_V-9eTBlhoMtb1OD6GuHv4oTcbLatufJ5WXMnU8",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTAyOTcwMWZiYmMxNTAwMmZiM2Y3ZTkiLCJpYXQiOjE2Mjc1NjM4MjMsImV4cCI6MTYzMDE1NTgyM30.SiJw_JXtwRHAUfcippuJ9QJgMowQ2bPW9DzjuHc7xy4",
       },
     };
 
     if (selected.length > 1) {
-      const response = await axios.post(
-        `http://localhost:8000/gateway/v1/tasks/assign`,
-        {
-          agent_id: agentId,
-          agent_name: agentName,
-          tasksIds: selected
-        },
-        config
+      const response = await axios.post(`http://localhost:8000/gateway/v1/tasks/assign`,
+      {
+        agent_id: agentId,
+        agent_name: agentName,
+        tasksIds: selected
+      },
+      config
       );
       alert(JSON.stringify(response, null, 2));
     } else {
-      const response = await axios.put(
-        `http://localhost:8000/gateway/v1/tasks/${selected[0]}/assign`,
+      const response = await axios.put(`http://localhost:8000/gateway/v1/tasks/${selected[0]}/assign`,
         {
           agent_id: agentId,
           agent_name: agentName,
@@ -427,18 +385,6 @@ const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <div className={classes.assignDiv}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => assignTask()}
-          >
-            ASSIGN TASK TO AGENT
-            <ListItemIcon>
-              <LabelImportantIcon />
-            </ListItemIcon>
-          </Button>
-        </div>
         <EnhancedTableToolbar numSelected={selected.length} />
         <TableContainer>
           <Table
@@ -520,6 +466,14 @@ const EnhancedTable: React.FC<TableProps> = ({dataRows}) => {
         control={<Switch checked={dense} onChange={handleChangeDense} />}
         label="Dense padding"
       />
+       <div className={classes.assignDiv}>
+          <Button variant="contained" color="primary" onClick={() => assignTask()}>
+            ASSIGN TASK
+            <ListItemIcon>
+              <LabelImportantIcon />
+            </ListItemIcon>
+          </Button>
+        </div>
     </div>
   );
 }
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -3,10 +3,12 @@ import Timeline from "react-calendar-timeline";
 import { FC, useState, useEffect } from "react";
 import PanelGroup from "react-panelgroup";
 import FullWidthTabs from "../components/FullWidthTabs";
-import { Button, ListItemIcon } from "@material-ui/core";
+import { Box, Button, ListItemIcon } from "@material-ui/core";
 import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
-import AlarmAdd from "@material-ui/icons/AlarmAdd";
+import UpdateIcon from '@material-ui/icons/Update';
+import DeleteIcon from '@material-ui/icons/Delete';
 import TaskData from "../components/TaskData";
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import moment from "moment";
 import { Data } from "../interfaces/tasks-data.interface";
 import { Task } from "../interfaces/tasks.interface";
@@ -113,16 +115,23 @@ const TaskPage: FC = () => {
       ],
     },
   });
-
+  const [showActions,setShowActions] = useState(false);
   const [loading, setLoading] = useState(false);
   const baseUrl = "http://localhost:8000/gateway/v1/";
+
+
+
+  const callSetShowActions = () => {
+    debugger
+    setShowActions(!showActions);
+  };
 
   const fetchTasksData = async () => {
     setLoading(true);
     const response = await axios.get(`${baseUrl}tasks/systems/3`, {
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MGU1YjI4NTQ3YTBmMDAwMmZiNWUzYzkiLCJpYXQiOjE2MjY2MDk0ODIsImV4cCI6MTYyOTIwMTQ4Mn0.gLHb_V-9eTBlhoMtb1OD6GuHv4oTcbLatufJ5WXMnU8",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTAyOTcwMWZiYmMxNTAwMmZiM2Y3ZTkiLCJpYXQiOjE2Mjc1NjM4MjMsImV4cCI6MTYzMDE1NTgyM30.SiJw_JXtwRHAUfcippuJ9QJgMowQ2bPW9DzjuHc7xy4",
       },
     });
 
@@ -157,100 +166,32 @@ const TaskPage: FC = () => {
         "loading"
       ) : (
         <div>
-          <Timeline
-            groups={groups}
-            items={createTimeline(tasks).createTimelineTasks}
-            defaultTimeStart={moment().add(-12, "hour")}
-            defaultTimeEnd={moment().add(12, "hour")}
-            onItemDoubleClick={(itemId, e, time) => {
-              //@ts-ignore
-              setSelectedTask(tasks.find((task) => task.id === itemId));
-            }}
-          />
-
+          <Timeline groups={groups} items={createTimeline(tasks).createTimelineTasks} 
+                    defaultTimeStart={moment().add(-12, "hour")} 
+                    defaultTimeEnd={moment().add(12, "hour")} 
+                    onItemDoubleClick={
+                    (itemId, e, time) => {
+                      //@ts-ignore
+                      setSelectedTask(tasks.find((task) => task.id === itemId));
+                    }}/>
+                    
+          <div className="floatL">
+            <Button color="primary"><ListItemIcon><LibraryAddIcon />NEW</ListItemIcon></Button>
+            <Box display={showActions} className="floatR">
+              <Button color="primary"><ListItemIcon><DeleteIcon />DELETE</ListItemIcon></Button>
+              <Button color="primary"><ListItemIcon><UpdateIcon />EDIT</ListItemIcon></Button>
+              <Button color="primary"><ListItemIcon><AssignmentIndIcon />ASSIGN</ListItemIcon></Button>
+            </Box>
+          </div>
           <PanelGroup spacing={5} panelWidths={[{ size: 800, minSize: 650},{ size: 440, minSize: 350}]}>
-            <TaskData dataRows={dataRows} />
+            <TaskData dataRows={dataRows} setButtons={callSetShowActions}/>
             <FullWidthTabs selectedTask={selectedTask} />
           </PanelGroup>
 
-          <div>
-            <Button variant="contained" color="primary">NEW TASK<ListItemIcon><LibraryAddIcon /></ListItemIcon>
-            </Button>
-          </div>
+         
         </div>
       )}
     </div>
   );
 };
 export default TaskPage;
-
-// const items = [
-//   {
-//     id: 1,
-//     group: 1,
-//     title: "קריאת מד",
-//     start_time: moment(),
-//     end_time: moment().add(1, "hour"),
-//     canMove: true,
-//     canResize: true,
-//     canChangeGroup: true,
-//     itemProps: {
-//       // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
-//       "data-custom-attribute": "Random content",
-//       "aria-hidden": true,
-//       onDoubleClick: () => {
-//         alert("Show task details");
-//       },
-//       className: "weekend",
-//       style: {
-//         background: "green",
-//       },
-//     },
-//   },
-//   {
-//     id: 2,
-//     group: 2,
-//     title: "התנקשות",
-//     start_time: moment().add(-0.5, "hour"),
-//     end_time: moment().add(0.5, "hour"),
-//     canMove: true,
-//     canResize: true,
-//     canChangeGroup: true,
-//     itemProps: {
-//       // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
-//       "data-custom-attribute": "Random content",
-//       "aria-hidden": true,
-//       onDoubleClick: () => {
-//         alert("Show task details");
-//       },
-//       className: "weekend",
-//       style: {
-//         background: "red",
-//       },
-//     },
-//   },
-//   {
-//     id: 3,
-//     group: 3,
-//     title: "גביה",
-//     start_time: moment().add(2, "hour"),
-//     end_time: moment().add(3, "hour"),
-//     canMove: true,
-//     canResize: true,
-//     canChangeGroup: true,
-//     itemProps: {
-//       // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
-//       "data-custom-attribute": "Random content",
-//       "aria-hidden": true,
-//       onDoubleClick: () => {
-//         alert("Show task details");
-//       },
-//       className: "weekend",
-//       style: {
-//         background: "blue",
-//       },
-//     },
-//   },
-// ];
-
-
