01a73e26217a552e597c32cfc4dfb03329bb2755
ItsikEdri <itsik.edri@metropolinet.co.il>
2021-08-02 09:00:12 +0300

    timeline

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,5 +1,9 @@
 
-import Timeline from "react-calendar-timeline";
+import Timeline , {
+  TimelineHeaders,
+  SidebarHeader,
+  DateHeader
+}from "react-calendar-timeline";
 import { FC, useState, useEffect } from "react";
 import PanelGroup from "react-panelgroup";
 import FullWidthTabs from "../components/FullWidthTabs";
@@ -16,8 +20,8 @@ import axios from "axios";
 import React from "react";
 
 const groups = [
-  { id: "Guy", title: "Guy", rightTitle: "side title", stackItems: true },
-  { id: "Itsik", title: "Itsik", rightTitle: "side title", stackItems: true },
+  { id: "Guy", title: "Guy", rightTitle: "Agent", stackItems: true },
+  { id: "Itsik", title: "Itsik", rightTitle: "Agent", stackItems: true },
 ];
 
 function createData(
@@ -75,7 +79,8 @@ const createTimeline = (tasks: Task[]) => {
         // },
         className: "weekend",
         style: {
-          background: "blue",
+          background: "lightgreen",
+          color: "black",
         },
       },
     };
@@ -89,36 +94,36 @@ const TaskPage: FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [selectedTask, setSelectedTask] = useState<Task>(
     {
-    assignment_info: {
-      agent_id: "",
-      agent_name: "",
-    },
-    created_at: "0",
-    description: "",
-    duration: 0,
-    name: "",
-    type: "",
-    updated_at: "0",
-    start_time: 0,
-    system: {
+      assignment_info: {
+        agent_id: "",
+        agent_name: "",
+      },
+      created_at: "0",
+      description: "",
+      duration: 0,
       name: "",
-      type: "0",
-    },
-    id: "",
-    state: {
-      availableStates: [],
-      stateHistory: [
-        {
-          createdAt: "",
-          currentState: "",
-          previousState: "",
-          updatedAt: "",
-        },
-      ],
-    },
+      type: "",
+      updated_at: "0",
+      start_time: 0,
+      system: {
+        name: "",
+        type: "0",
+      },
+      id: "",
+      state: {
+        availableStates: [],
+        stateHistory: [
+          {
+            createdAt: "",
+            currentState: "",
+            previousState: "",
+            updatedAt: "",
+          },
+        ],
+      },
     }
   );
-  const [showActions,setShowActions] = useState(true);
+  const [showActions, setShowActions] = useState(true);
   const [loading, setLoading] = useState(false);
   const baseUrl = "http://localhost:8000/gateway/v1/";
 
@@ -131,7 +136,7 @@ const TaskPage: FC = () => {
     const response = await axios.get(`${baseUrl}tasks/systems/3`, {
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTAyODJlZDM2ZWU3NzAwMzFmODQzYmIiLCJpYXQiOjE2Mjc4MDY4NDQsImV4cCI6MTYzMDM5ODg0NH0.JVESKVFnV1GySLzeNXQah19ZRuMwE9FNtyKwCgcOCkk",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTA2YTBhOWY2YTA3NTAwMzBjYzY4OTIiLCJpYXQiOjE2Mjc4MjQzNDYsImV4cCI6MTYzMDQxNjM0Nn0.IXVQnjVkH42L-fUBpU4qD5_YabUEpdSaxMjq8VJnUQA",
       },
     });
 
@@ -169,20 +174,48 @@ const TaskPage: FC = () => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const onTaskDrag = (itemDragObject: any) => {
+    if (itemDragObject.eventType === "move") {
+      console.log('set new object to time line with time: ' + itemDragObject.time.toString());
+    } else if (itemDragObject.eventType === "resize") {
+      
+      console.log('set new object to time line with dureation to the: '+ itemDragObject.edge + itemDragObject.time.toString());
+    }
+  };
+
+
   return (
     <div>
       {loading ? (
         "loading"
       ) : (
         <div>
-          <Timeline groups={groups} items={createTimeline(tasks).createTimelineTasks} 
-                    defaultTimeStart={moment().add(-12, "hour")} 
-                    defaultTimeEnd={moment().add(12, "hour")} 
-                    onItemDoubleClick={
-                    (itemId, e, time) => {
-                      //@ts-ignore
-                      setSelectedTask(tasks.find((task) => task.id === itemId));
-                    }}/>
+            <Timeline groups={groups} items={createTimeline(tasks).createTimelineTasks}
+              sidebarWidth={100}
+              rightSidebarContent={'Agents'}
+              canResize={"both"}
+              onItemDrag={(itemDragObject) => {
+                //@ts-ignore
+                onTaskDrag(itemDragObject);
+              }}
+              defaultTimeStart={moment().add(-12, "hour")} 
+              defaultTimeEnd={moment().add(12, "hour")} 
+              onItemSelect={
+              (itemId, e, time) => {
+                //@ts-ignore
+                setSelectedTask(tasks.find((task) => task.id === itemId));
+                }}>
+              <TimelineHeaders>
+                <SidebarHeader>
+                  {({ getRootProps }) => {
+                    return <div {...getRootProps()}>AGENTS</div>
+                  }}
+                </SidebarHeader>
+                  <DateHeader unit="primaryHeader" />
+                  <DateHeader />
+              </TimelineHeaders>
+              </Timeline>
                     
           <div className="floatL">
             <Button color="primary"><ListItemIcon><LibraryAddIcon />NEW</ListItemIcon></Button>
